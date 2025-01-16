c751e413da515c0427c344a00ec59ff9717b5413
Will Lumley <will@lumley.io>
2025-01-16 17:15:08 +1100

    Created project

---
+++ DocuBot/DocuBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocuBotApp.swift
+//  DocuBot
+//
+//  Created by William Lumley on 3/7/2024.
+//
+
+import SwiftUI
+
+@main
+struct DocuBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
