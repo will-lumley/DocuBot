5654a07928bd2465babc7766b2a7e20467e07497
Will Lumley <will@lumley.io>
2025-01-16 17:15:13 +1100

    Added DocC to Toolbox.

--- DocuBotToolbox/Sources/DocuBotToolbox/Error+Description.swift
+++ DocuBotToolbox/Sources/DocuBotToolbox/Error+Description.swift
@@ -7,8 +7,16 @@
 
 import Foundation
 
+/// An extension on `Error` to provide a custom description of the error.
 public extension Error {
-
+    
+    /// A string representation of the error.
+    ///
+    /// This property prioritises a more detailed, human-readable description of the error:
+    /// 1. If the error conforms to `LocalizedError` and provides an `errorDescription`, it returns that value.
+    /// 2. Otherwise, it falls back to the `localizedDescription` of the error.
+    ///
+    /// - Returns: A `String` describing the error.
     var description: String {
         if
             let localError = self as? LocalizedError,
--- DocuBotToolbox/Sources/DocuBotToolbox/String+Checksum.swift
+++ DocuBotToolbox/Sources/DocuBotToolbox/String+Checksum.swift
@@ -7,8 +7,15 @@
 
 import CryptoKit
 
+/// An extension on `String` to provide a computed property for generating a checksum using SHA-256.
 public extension String {
-
+    
+    /// A SHA-256 checksum of the string.
+    ///
+    /// This property computes the SHA-256 hash of the string and returns it as a hexadecimal string.
+    /// If the string cannot be converted to UTF-8 data, the property returns `nil`.
+    ///
+    /// - Returns: A `String` containing the hexadecimal representation of the SHA-256 hash, or `nil` if the string cannot be converted to data.
     var checksum: String? {
         // Convert the combined content to data
         guard let contentData = self.data(using: .utf8) else {
